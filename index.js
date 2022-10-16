3a74fb0f8a31a0b552aece9510b27d4babbe489a
vikasairsharma7071 <57059631+vikasairsharma7071@users.noreply.github.com>
2022-10-16 13:47:00 +0530

    Add initial files of drum kit

---
+++ index.js
@@ -0,0 +1,88 @@
+
+// document.querySelector("button").addEventListener("click",myClick);
+
+for(var i=0;i<(document.querySelectorAll(".drum").length);i++){
+    document.querySelectorAll("button")[i].addEventListener("click",myClick);
+}
+
+
+
+function myClick(){
+    var buttonClicked = this.innerHTML;
+
+    makeSound(buttonClicked);
+    buttonAnimation(buttonClicked);
+    
+}
+
+
+// var audio = new Audio('/sounds/tom-1.mp3');
+//     audio.play();
+
+
+// this.style.color = "white";
+
+
+
+
+
+
+document.addEventListener("keydown",keybordOne);
+
+
+function keybordOne(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+    // alert("The key which got pressed is "+event.key);
+}
+
+
+function makeSound(buttonClicked){
+    switch (buttonClicked) {
+        case "w":
+            var audio = new Audio('/sounds/crash.mp3');
+            audio.play();
+            break;
+             
+        case "a":
+            var audio = new Audio('/sounds/kick-bass.mp3');
+            audio.play();
+            break;    
+        case "s":
+            var audio = new Audio('/sounds/snare.mp3');
+            audio.play();
+            break;
+             
+        case "d":
+            var audio = new Audio('/sounds/tom-1.mp3');
+            audio.play();
+            break;   
+        case "j":
+            var audio = new Audio('/sounds/tom-2.mp3');
+            audio.play();
+            break;
+             
+        case "k":
+            var audio = new Audio('/sounds/tom-3.mp3');
+            audio.play();
+            break;    
+
+        case "l":
+            var audio = new Audio('/sounds/tom-4.mp3');
+            audio.play();
+            break;                                  
+    
+        default:
+            
+    }
+}
+
+
+function buttonAnimation(buttonClicked){
+    var activeButton = document.querySelector("."+buttonClicked);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function (){
+        activeButton.classList.remove("pressed",200);
+    },100);
+}
